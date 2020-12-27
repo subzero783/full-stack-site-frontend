2ab002cb711b9f9f842ed3438a763f013699cac8
Gus <gusta1296@gmail.com>
2020-12-27 13:27:38 -0800

    first push

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
+import NotFoundPage from './pages/NotFoundPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <div id="page-body">
+          <Switch>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed tellus risus. Vestibulum id ligula non velit scelerisque fermentum. Cras nibh urna, molestie ut est porttitor, pretium mollis sapien. Ut maximus dolor quis porttitor pellentesque. Morbi justo massa, fermentum at lorem tincidunt, suscipit mattis lacus. Nulla ullamcorper, odio sit amet tempus consectetur, risus odio congue odio, eget condimentum velit erat vitae leo. In bibendum euismod ultricies. Quisque tincidunt posuere tellus, sed venenatis turpis iaculis at. Mauris iaculis, eros vitae ornare finibus, velit est bibendum leo, sagittis fermentum dui nulla a elit. Proin ut tempor elit. Etiam suscipit, magna eu porttitor efficitur, erat nibh placerat lacus, nec facilisis turpis nibh vitae ligula. Suspendisse potenti. Vestibulum pharetra ligula eu orci lacinia, ut tempor ex egestas.
+
+    Vestibulum imperdiet ut felis non blandit. In porta pulvinar turpis ut pellentesque. Quisque ac tempor est, a iaculis elit. Nam pulvinar eu purus id condimentum. Donec ornare rhoncus arcu. Maecenas facilisis sapien et diam maximus, nec egestas tortor rutrum. Nam sit amet orci pharetra, facilisis leo ut, tempor nisi. Aliquam vehicula vitae quam vitae iaculis. Vestibulum ut orci bibendum, accumsan nunc vel, auctor velit. Nulla sagittis elit et iaculis pharetra. Vivamus quis turpis quis est molestie tempor id eget enim. Praesent ac rhoncus risus.</p>
+  </>
+);
+
+export default HomePage;
